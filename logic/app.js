98d9e477a952b32324f04f1fa315d0573d659b44
Eugene Rinaldi <eugene@sifters.net>
2023-10-16 04:42:20 -0500

    update default level because lazy

--- logic/app.js
+++ logic/app.js
@@ -53,7 +53,7 @@ var minutes 		= 0;  // tracks the number of minutes the current test has been ru
 var gameOn 			= false; // set to true when user starts typing in input field
 var correct 		= 0;  // number of correct keystrokes during a game
 var errors 			= 0;  // number of typing errors during a game
-var currentLevel 	= localStorage.getItem('currentLevel') || 1; // int representation of the current level, which determines which letter set to test
+var currentLevel 	= localStorage.getItem('currentLevel') || 2; // int representation of the current level, which determines which letter set to test
 var correctAnswer;        // string representation of the current correct word
 var letterIndex 	= 0;  // Keeps track of where in a word the user is
 					      // Increment with every keystroke except ' ', return, and backspace
